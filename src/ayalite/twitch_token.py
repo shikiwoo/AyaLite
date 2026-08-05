d9de2d958b9134abe78902cb86c1a3c0f383d43a
shiki <shiki@shikiwoo.com>
2026-08-05 06:56:44 +0200

    feat: implement token invalidation

--- src/ayalite/twitch_token.py
+++ src/ayalite/twitch_token.py
@@ -4,7 +4,7 @@ TOKEN_URL = "https://id.twitch.tv/oauth2/token"
 
 class TokenManager:
     def __init__(self, client_id, client_secret, session) -> None:
-        self._client = client_id
+        self.client = client_id
         self._secret = client_secret
         self.session = session
         self._token = None
@@ -17,7 +17,7 @@ class TokenManager:
 
     async def _fetch(self) -> None:
         payload = {
-            "client_id": self._client,
+            "client_id": self.client,
             "client_secret": self._secret,
             "grant_type": "client_credentials",
         }
@@ -32,3 +32,6 @@ class TokenManager:
             # parse the response. subtract 300 seconds from the expiry to make sure the token is always valid.
             self._token = body["access_token"]
             self._expiry = time.monotonic() + body["expires_in"] - 300
+
+    def invalidate(self):
+        self._token = None
